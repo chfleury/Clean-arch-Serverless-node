fd072d91d7bd4c90cfbf463c4797a57c841e458f
Christian Siqueira <chfleurysiq@gmail.com>
2024-04-28 09:51:53 -0300

    feat: implement Validatable class

---
+++ src/adapters/validators/util/Validatable.ts
@@ -0,0 +1,44 @@
+import {
+  ValidationError as ClassValidationError,
+  validateSync,
+} from "class-validator";
+
+export type ValidationError = {
+  errors: string[];
+};
+
+export abstract class Validatable<A> {
+  constructor(private data: Partial<A>) {}
+
+  export(): Partial<A> {
+    return this.data;
+  }
+
+  checkForErrors(): ValidationError | null {
+    const validationResult = validateSync(this.data);
+
+    // TODO REFACTOR
+    if (
+      validationResult instanceof Array &&
+      validationResult.length &&
+      validationResult[0] instanceof ClassValidationError
+    ) {
+      const validationErrors: string[] = [];
+      validationResult.forEach((obj) => {
+        const constraints = obj.constraints;
+
+        for (const key in constraints) {
+          if (typeof constraints?.[key] === "string" && constraints?.[key]) {
+            validationErrors.push(constraints?.[key]!);
+          }
+        }
+      });
+
+      return {
+        errors: validationErrors,
+      };
+    }
+
+    return null;
+  }
+}
