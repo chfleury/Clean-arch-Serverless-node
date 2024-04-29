0deed3f0b1a1a7ad089578f0b09c3b41a88dd772
Christian Siqueira <chfleurysiq@gmail.com>
2024-04-28 21:01:33 -0300

    test:  should return 400 if body is missing

--- src/tests/unit/syncChallengeLambdaHandler.test.ts
+++ src/tests/unit/syncChallengeLambdaHandler.test.ts
@@ -2,8 +2,8 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { syncChallengeLambdaHandler } from '../../app';
 import { expect, describe, it } from '@jest/globals';
 
-describe('Unit test for app', function () {
-    it('verifies successful response', async () => {
+describe('SyncChallengeLambdaHandler', function () {
+    it('should return status 200 if everything goes well', async () => {
         const inputBody = JSON.stringify({
             firstName: 'John',
             lastName: 'Doe',
@@ -72,5 +72,63 @@ describe('Unit test for app', function () {
 
         expect(result.statusCode).toEqual(200);
         expect(result.body).toEqual(inputBody);
+    });
+
+    it('should return 400 if body is missing', async () => {
+        const inputBody = null;
+
+        const event: APIGatewayProxyEvent = {
+            httpMethod: 'post',
+            body: inputBody,
+            headers: {},
+            isBase64Encoded: false,
+            multiValueHeaders: {},
+            multiValueQueryStringParameters: {},
+            path: '/hello',
+            pathParameters: {},
+            queryStringParameters: {},
+            requestContext: {
+                accountId: '123456789012',
+                apiId: '1234',
+                authorizer: {},
+                httpMethod: 'get',
+                identity: {
+                    accessKey: '',
+                    accountId: '',
+                    apiKey: '',
+                    apiKeyId: '',
+                    caller: '',
+                    clientCert: {
+                        clientCertPem: '',
+                        issuerDN: '',
+                        serialNumber: '',
+                        subjectDN: '',
+                        validity: { notAfter: '', notBefore: '' },
+                    },
+                    cognitoAuthenticationProvider: '',
+                    cognitoAuthenticationType: '',
+                    cognitoIdentityId: '',
+                    cognitoIdentityPoolId: '',
+                    principalOrgId: '',
+                    sourceIp: '',
+                    user: '',
+                    userAgent: '',
+                    userArn: '',
+                },
+                path: '/hello',
+                protocol: 'HTTP/1.1',
+                requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
+                requestTimeEpoch: 1428582896000,
+                resourceId: '123456',
+                resourcePath: '/hello',
+                stage: 'dev',
+            },
+            resource: '',
+            stageVariables: {},
+        };
+
+        const result: APIGatewayProxyResult = await syncChallengeLambdaHandler(event);
+
+        expect(result.statusCode).toEqual(400);
     });
 });
